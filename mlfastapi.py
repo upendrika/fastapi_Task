9f5ead4131beaa31bf80d51ff1675d92d8fa2651
ishara upendrika <145433182+upendrika@users.noreply.github.com>
2024-09-13 11:54:04 +0530

    testing1

--- mlfastapi.py
+++ mlfastapi.py
@@ -5,14 +5,14 @@ import pickle
 
 app = FastAPI(debug=True)
 
-
+# Load the model and label encoders when the app starts
 with open('saved_steps.pkl', 'rb') as file:
     data = pickle.load(file)
     model = data['model']
     le_country = data['le_country']
     le_education = data['le_education']
 
-
+# Define a Pydantic model for the POST request body
 class SalaryPredictionRequest(BaseModel):
     country: str
     education_level: str
@@ -20,18 +20,17 @@ class SalaryPredictionRequest(BaseModel):
 
 @app.get('/')
 def home():
-    return {'text': 'Software Developer Salary Prediction!'}
+    return {'text': 'Software Developer Salary Prediction! 2024'}
 
 @app.get('/calculate_salary')
 def predict_get(country: str, education_level: str, years_of_experience: int):
-   
+  
     country_encoded = le_country.transform([country])[0]
     education_encoded = le_education.transform([education_level])[0]
     input_data = [[country_encoded, education_encoded, years_of_experience]]
     
     prediction = model.predict(input_data)
     output = round(prediction[0], 2)
-
     return {'The estimated salary is {}'.format(output)}
 
 @app.post('/predict_salary')
@@ -39,14 +38,11 @@ def predict_post(request: SalaryPredictionRequest):
 
     country_encoded = le_country.transform([request.country])[0]
     education_encoded = le_education.transform([request.education_level])[0]
-    
     input_data = [[country_encoded, education_encoded, request.years_of_experience]]
-    
     prediction = model.predict(input_data)
     output = round(prediction[0], 2)
 
     return {'The estimated salary is {}'.format(output)}
-
 
 if __name__ == '__main__':
     uvicorn.run("mlfastapi:app", host="0.0.0.0", port=8000, reload=True)
